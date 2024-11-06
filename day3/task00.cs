97ed78ec30e16e5876d84270c8c37a424a9e3b58
Agnes <agnes.vj28@gmail.com>
2024-11-06 10:32:29 +0000

    add new class task00

---
+++ day3/task00.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+    internal class task00
+    {
+        public static void Run()
+        {
+            Console.WriteLine("This works!!");
+        }
+    }
+}
